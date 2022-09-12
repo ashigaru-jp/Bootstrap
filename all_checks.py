ddfa15ef560d8716954552427bcd004de5961916
ashigaru-jp <110610546+ashigaru-jp@users.noreply.github.com>
2022-09-12 21:03:31 +0900

    catching up with the tutorial.

--- all_checks.py
+++ all_checks.py
@@ -10,6 +10,7 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print "Everythiing ok."
         sys.exit(0)
 
